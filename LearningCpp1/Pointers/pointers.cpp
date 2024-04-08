a373a555e02a70cba4bc182ed966ca7691ef1a03
lhalcomb <lhalcomb@drury.edu>
2024-04-08 00:52:34 -0500

    playing with pointers and beginning linked list

--- LearningCpp1/Pointers/pointers.cpp
+++ LearningCpp1/Pointers/pointers.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
+using namespace std;
+
+void swapv(int *initial_num, int *new_number){
+
+  int temp;
+  temp = *initial_num;
+  *initial_num = *new_number;
+  *new_number = temp;
+
+}
 int main()
 {
     int* p;
@@ -8,13 +18,33 @@ int main()
       Unlike variables, pointers are types that can hold the address of a particular object.
     */
 
-   int x = 123;
-   int* n = &x; //This points to the existing object in memory denoted as x. We use the address-of operator &, and say that n points to x
+   int num = 123;
+   int *num_address = &num; //This points to the existing object in memory denoted as x. We use the address-of operator &, and say that n points to x
 
-   char* c = nullptr; //This declares a pointer variable of a specified empty spot in memory
+   char *anEmptyPointer = nullptr; //This declares a pointer variable of a specified empty spot in memory
 
-   std::cout<< "The value of the dereferenced pointer is: " <<*n << '\n'; 
+   std::cout<< "The value of the dereferenced pointer is: " << *num_address << '\n'; 
 
-   *n = x * 2;
-   std::cout <<"The value of the dereferenced pointer is now: " <<*n;
+   *num_address = num * 2;
+   std::cout <<"The value of the dereferenced pointer is now: " << *num_address;
+
+   int anotherNum = 44;
+   int *anotherNum_address;
+
+   anotherNum_address = &anotherNum;
+
+   std::cout << "\n " << anotherNum_address <<endl;
+
+   int num1, num2;
+   cout << "First Number: " << endl;
+   cin >> num1;
+
+   cout << "Second Number: " <<endl;
+   cin >> num2;
+
+   swapv(&num1, &num2);
+
+   cout <<"After swapping, first number is : " << num1 <<endl;
+   cout << "After  swapping, second number is : " << num2 << endl;
+
 }
---
+++ LinkedList/SinglyLinkedList.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+struct node{
+    int data;
+    node *next;
+};
+
+class SinglyLinkedList{
+    private:
+        node* head; //head of the list
+
+}
