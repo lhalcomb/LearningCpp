413cf91417c096b06d3315a2f925533ad49f90d1
lhalcomb <lhalcomb@drury.edu>
2024-05-08 23:30:26 -0500

    Playing around with raylib again

--- RaylibFun/OpenWindow.cpp
+++ RaylibFun/OpenWindow.cpp
@@ -1,14 +1,22 @@
 #include <raylib.h>
 
 int main(){
-    InitWindow(400, 600, "My First RayLib Window");
+    const int screenWidth = 400;
+    const int screenHeight = 600;
+
+    InitWindow(screenWidth, screenHeight, "My First RayLib Window");
 
     while (!WindowShouldClose()){
         BeginDrawing(); // Begin drawing frame
 
         ClearBackground(RAYWHITE); // Clear the background with a specific color (here, light gray)
 
-        DrawLine(0, 0, GetScreenWidth(), GetScreenHeight(), BLACK); // Draw the diagonal line
+        //DrawLine(0, 0, GetScreenWidth(), GetScreenHeight(), BLACK); // Draw the diagonal line
+
+        float widthOfScreen = static_cast<float>(screenWidth);
+        float heightOfScreen = static_cast<float>(screenHeight);
+        DrawLineEx((Vector2){0.0f,0.0f}, (Vector2){widthOfScreen, heightOfScreen}, 5, GREEN);   // Draw a line (using triangles/quads)
+
 
         EndDrawing(); // End of frame
 
