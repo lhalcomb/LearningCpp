6cac5fda573f3767870c521a2459eb99eaf5404c
lhalcomb <lhalcomb@drury.edu>
2024-08-22 10:33:16 -0500

    getstuff

--- LearningCpp1/Arrays/initArrays.cpp
+++ LearningCpp1/Arrays/initArrays.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 
+using namespace std;
+
 int arr[5] = { 1, 2, 3, 4, 5 }; //This is how you would declare a list of 5 elements
 
 int main(){
-    for (int i = 0; i < 5; i++){
+    /* for (int i = 0; i < 5; i++){
         std::cout <<i;
     }
+
+    int arr[5]; */
+
+    char a = 'A';
+    a++;
+    std::cout << static_cast<int>(a);
+    std::cout <<static_cast<char>(a);
 }
 
