caebde86f4409042a97178e83b24cc1a005ff6e9
lhalcomb <lhalcomb@drury.edu>
2024-08-29 12:08:46 -0500

    Playing with types again.

--- LearningCpp1/Scope/ScopeEx.cpp
+++ LearningCpp1/Scope/ScopeEx.cpp
@@ -1,2 +1,25 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
+
+int main() {
+    const double PI = 3.14159;
+
+    //PI = 3;
+
+    std::cout << PI << std::endl;
+
+    std::vector<int> list = {1, 2, 3};
+    int a = list[1];
+    float b = static_cast<float>(a);
+
+    std::string greeting = "Hello";
+
+    //greeting = 1; 
+
+    std::cout << greeting << std::endl;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+
+}
